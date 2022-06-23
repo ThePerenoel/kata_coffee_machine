c7afebe2b70673e8244007f52fa6854041493492
loicn <loicnoel12@gmail.com>
2022-06-23 15:45:05 +0200

    Add test for hot chocolate with 1 sugar

--- src/main/java/Drink.java
+++ src/main/java/Drink.java
@@ -1,11 +1,16 @@
 public class Drink {
     private DrinkType drinkType;
+    private int numberOfSugar;
 
-    public Drink(DrinkType drinkType) {
+    public Drink(DrinkType drinkType, int numberOfSugar) {
         this.drinkType = drinkType;
+        this.numberOfSugar = numberOfSugar;
     }
 
     public String getStringCommand() {
-        return "H::";
+        if (numberOfSugar == 0) {
+            return "H::";
+        }
+        return "H:" + numberOfSugar + ":0";
     }
 }
--- src/test/java/DrinkTest.java
+++ src/test/java/DrinkTest.java
@@ -13,8 +13,19 @@ class DrinkTest {
         @Test
         @DisplayName("return H:: given hot chocolate with no sugar")
         void return_H_given_hot_chocolate_with_no_sugar() {
-            Drink hotChocolateWithoutSugar = new Drink(DrinkType.HOT_CHOCOLATE);
+            Drink hotChocolateWithoutSugar = new Drink(DrinkType.HOT_CHOCOLATE, 0);
             String expectedStringCommand = "H::";
+
+            String stringCommand = hotChocolateWithoutSugar.getStringCommand();
+
+            assertThat(stringCommand).isEqualTo(expectedStringCommand);
+        }
+
+        @Test
+        @DisplayName("return H:1:0 given hot chocolate with one sugar")
+        void return_H10_given_hot_chocolate_with_one_sugar() {
+            Drink hotChocolateWithoutSugar = new Drink(DrinkType.HOT_CHOCOLATE, 1);
+            String expectedStringCommand = "H:1:0";
 
             String stringCommand = hotChocolateWithoutSugar.getStringCommand();
 
